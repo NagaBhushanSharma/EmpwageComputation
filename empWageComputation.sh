2ce8d124713d50ae1cc124a7a9548fc3356a734b
NagaBhushanSharma <mnbs929@gmail.com>
2021-02-16 16:20:52 +0530

    added EmpwageComputation

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -x
+
 #UC1
 echo "Hello Welcome to EmpDeatils Programe"
+
 isPresent=1;
 wagePerHr=20;
 fullDay=8;
@@ -8,21 +10,23 @@ dailyPayment=0;
 fulltime=8;
 parttime=5;
 checkRandom=$((RANDOM%2))
-if [ $isPresent -eq $checkRandom ]
+partFullCheck=$((RANDOM%2))  #UC-4 (0-FullTime 1-PartTime)
+if [ $isPresent -eq $checkRandom ]   
 then
      echo "Emp is Present"
-      if [ $fulltime -eq 8 ]
-      then
-         #UC2
-         dailyPayment=$(($wagePerHr*$fulltime))
+            #UC-4
+      case $partFullCheck in
+            #UC-2
+         0) dailyPayment=$(($wagePerHr*$fulltime))
             echo "Daily Payment is:" $dailyPayment
-      else
-          #UC3
-         dailyPayment=$(($wagePerHr*$parttime))
+          ;;     
+             #UC-3
+         1)  dailyPayment=$(($wagePerHr*$parttime))
             echo "Daily Payment is:" $dailyPayment
-fi         
+          ;;
+      esac
+
 else
     echo "Emp is Absent"
     echo "Daily Payment is:" $dailyPayment
 fi
-
